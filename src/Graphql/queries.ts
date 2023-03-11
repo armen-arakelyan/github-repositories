ef1fbe466110bb5c225fb6819d73b1d86f29c24b
armen-arakelyan <arakelyan2001@mail.ru>
2023-03-11 18:01:26 +0400

    Viewer data add

--- src/Graphql/queries.ts
+++ src/Graphql/queries.ts
@@ -1,23 +1,23 @@
 import { gql } from "@apollo/client";
 
 export const LOAD_REPOSITORY = gql`
-query GetRepositoryInfo($name: String!, $owner: String!) {
-  repository(name: $name, owner: $owner) {
-    name
-    stargazerCount
-    pushedAt
-    shortDescriptionHTML
-    owner {
-      login
-      avatarUrl
-    }
-    languages(first: 100) {
-      nodes {
-        name
+  query GetRepositoryInfo($name: String!, $owner: String!) {
+    repository(name: $name, owner: $owner) {
+      name
+      stargazerCount
+      pushedAt
+      shortDescriptionHTML
+      owner {
+        login
+        avatarUrl
+      }
+      languages(first: 100) {
+        nodes {
+          name
+        }
       }
     }
   }
-}
 `;
 
 export const LOAD_REPOSITORIES = gql`
@@ -52,6 +52,36 @@ export const LOAD_REPOSITORIES = gql`
         endCursor
         hasNextPage
         startCursor
+      }
+    }
+  }
+`;
+
+export const LOAD_VIEWER_REPOSITORIES = gql`
+  query GetViewerRepositories($first: Int!, $after: String) {
+    viewer {
+      repositories(first: $first, after: $after) {
+        totalCount
+        edges {
+          cursor
+          node {
+            ... on Repository {
+              id
+              name
+              stargazerCount
+              pushedAt
+              url
+              owner {
+                login
+              }
+            }
+          }
+        }
+        pageInfo {
+          endCursor
+          hasNextPage
+          startCursor
+        }
       }
     }
   }
--- src/redux/Repositories/action.ts
+++ src/redux/Repositories/action.ts
@@ -1,5 +1,8 @@
 import slice from "./slice";
-import { LOAD_REPOSITORIES } from "../../Graphql/queries";
+import {
+  LOAD_REPOSITORIES,
+  LOAD_VIEWER_REPOSITORIES,
+} from "../../Graphql/queries";
 import { AppDispatch, ErrorMessage, Edges } from "../../types";
 import { apolloClient } from "../../helpers";
 
@@ -7,18 +10,24 @@ export const fetchData =
   (searchQuery: string, cursor?: string) =>
   async (dispatch: AppDispatch): Promise<void> => {
     try {
+      const showViewerData = !!searchQuery;
       dispatch(slice.actions.startLoading());
       const { data } = await apolloClient.query({
-        query: LOAD_REPOSITORIES,
+        query: showViewerData ? LOAD_REPOSITORIES : LOAD_VIEWER_REPOSITORIES,
         variables: { searchQuery, first: 10, after: cursor || null },
       });
-      const { edges = [], repositoryCount = 0, pageInfo } = data.search;
+      const { edges = [], pageInfo } = showViewerData
+        ? data.search
+        : data.viewer.repositories;
       const { endCursor = "", hasNextPage = false } = pageInfo;
       const nodes = edges.map((edge: Edges) => edge.node);
+      const resositoriesCount = showViewerData
+        ? data.search.repositoryCount
+        : data.viewer.repositories.totalCount;
       dispatch(
         slice.actions.fetchDataSuccess({
           repositories: nodes,
-          repositoryCount,
+          repositoryCount: resositoriesCount,
           endCursor,
           hasNextPage,
           edges,
